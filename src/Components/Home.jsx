34ba54f372173a9da7edeb9ec7f9e4ae835da890
Arun <arundevweb7@outlook.com>
2025-08-02 16:27:58 +0530

    Updated:optimised visual

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,12 @@
 function Home() {
   return (
     <section id="home">
-      <img src="/coding3.jpg" className="home-image"></img>
+      <img src="/coding2.jpg" className="home-image"></img>
       <div className="home-overlay">
         
         <h1>ARUN KUMAR.R</h1>
-        <p>Web Developer | Programmer | Tech Enthusiast</p>
+        <p>I'm a passionate Web Developer | Programmer </p>
+        <a href="#contact" class="cta-btn">Hire Me</a>
       </div>
     </section>
   );
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -5,20 +5,20 @@ function Projects() {
     <p>Here are some of my recent projects that showcase my skills and creativity.</p>
     <div className="projects-container">
       <div className="project-card">
-        <h3>Portfolio Website 1</h3>
+        <h3>Portfolio Website </h3>
         <img src="/6060376.jpg" alt="Project 1" class="project-image" loading="lazy" />
         <p>A personal portfolio website showcasing my skills and projects.</p>
        <a href="www.google.com" target="_blank" rel="noopener noreferrer">View Project</a>
 
       </div>
       <div className="project-card">
-        <h3>Portfolio Website 2</h3>
+        <h3>Portfolio Website </h3>
         <img src="/6060376.jpg" alt="Project 2" class="project-image" loading="lazy" />
         <p>A personal portfolio website showcasing my skills and projects.</p>
    <a href="www.google.com" target="_blank" rel="noopener noreferrer">View Project</a>
       </div>
       <div className="project-card">
-        <h3>Portfolio Website 4</h3>
+        <h3>Portfolio Website </h3>
         <img src="/2517915.jpg" alt="Project 4" class="project-image" loading="lazy" />
         <p>A personal portfolio website showcasing my skills and projects.</p>
        <a href="www.google.com" target="_blank" rel="noopener noreferrer">View Project</a>
